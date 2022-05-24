e244e890875c8c046b0543cf0537b48b5db78320
OpenJNY <openjny@gmail.com>
2022-05-24 15:22:21 +0900

    add: start_afw cmdlet
    start_afw lets you start deallocated Azure Firewalls.

--- utils/azure.ps1
+++ utils/azure.ps1
@@ -21,6 +21,33 @@ function start_appgw {
     }
 }
 
+function start_afw {
+    # if ((test az) && (test fzf) && (test jq)) {
+    #     $target_afw = $(az network firewall list | jq -c '.[] | select(.ipConfigurations == []) | {name, resourceGroup}' | fzf)
+    #     $rg = $($target_afw | jq -r '.resourceGroup')
+    # } else {
+    $afw_metadata = Get-AzFirewall | Where-Object IpConfiguration -eq $Null | Select-Object Name, ResourceGroupName, Location | Out-GridView -PassThru
+    $afw_metadata | ForEach-Object {
+        # select PIPs
+        $pip_metadata = Get-AzPublicIpAddress -ResourceGroupName $_.ResourceGroupName `
+            | Where-Object IpConfiguration -eq $Null `
+            | Where-Object Location -eq $_.Location `
+            | Select-Object Name, ResourceGroupName, Location `
+            | Out-GridView -PassThru
+        # select VNets
+        $vnet_metadata = Get-AzVirtualNetwork -ResourceGroupName $_.ResourceGroupName `
+            | Where-Object Location -eq $_.Location `
+            | Select-Object Name, ResourceGroupName, Location `
+            | Out-GridView -PassThru
+        
+        $azfw = Get-AzFirewall -Name $_.Name -ResourceGroupName $_.ResourceGroupName
+        $vnet = Get-AzVirtualNetwork -Name $vnet_metadata.Name -ResourceGroupName $vnet_metadata.ResourceGroupName
+        $pip = $pip_metadata | ForEach-Object { Get-AzPublicIpAddress -Name $_.Name -ResourceGroupName $_.ResourceGroupName }
+        $azfw.Allocate($vnet, $pip)
+        Set-AzFirewall -AzureFirewall $azfw 
+    }
+}
+
 Set-Alias azsub Select-MyAzSubscription
 Set-Alias azlogin Login-AzAccount
 Set-Alias azexit Logout-AzAccount
