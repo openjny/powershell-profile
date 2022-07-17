54f3e27c39da7fe051b3301404763548e525e6e6
OpenJNY <openjny@gmail.com>
2022-07-17 14:51:44 +0900

    Split jupyter command based on base images

--- utils/docker.ps1
+++ utils/docker.ps1
@@ -1,3 +1,6 @@
-function docker-juypterlab () {
+function jupyterlab-base-notebook () {
     docker run -it --rm -p 8888:8888 -v ${PWD}:/home/jovyan/work jupyter/base-notebook:latest
+}
+function jupyterlab-scipy-notebook () {
+    docker run -it --rm -p 8888:8888 -v ${PWD}:/home/jovyan/work jupyter/scipy-notebook:latest
 }
