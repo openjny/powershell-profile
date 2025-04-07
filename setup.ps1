50c1431bda994870a50b876d1939f66c3e26626b
openjny <openjny@gmail.com>
2025-04-07 18:27:09 +0900

    fix: correct scoop installation and improve symlink creation in setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,14 +1,25 @@
 $script_path = $MyInvocation.MyCommand.Path
 $script_dir = Split-Path $script_path
 
-scoop install starship
-scoop install fzf
-scoop isntall neovim
+# Require scoop
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+    Write-Host "Scoop is not installed. Please install Scoop first."
+    exit 1
+}
+
+# Install dependencies
+scoop install starship fzf neovim zoxide
 Install-Module PSFzf -Scope CurrentUser
 # Install-Module posh-git -Scope CurrentUser
 
-# symlink
-New-Item -Path $HOME\.config\starship.toml -ItemType SymbolicLink -Value $(Join-Path $script_dir starship.toml)
+# Create symlink to starship.toml
+if (!(Test-Path $HOME\.config\starship.toml)) {
+    Write-Host "Creating symlink for starship.toml ($HOME\.config\starship.toml)"
+    Start-Process "cmd.exe" -ArgumentList "/c mklink $HOME\.config\starship.toml $script_dir\starship.toml" -Verb runas
+}
 
-# localfile
-New-Item -Path $HOME\profile.local.ps1 -ItemType File
+# touch a Tlcalfilprofile  e if it doesn't exist
+if (!(Test-Path $HOME\profile.local.ps1)) {
+    Write-Host "Creating profile.local.ps1 ($HOME\profile.local.ps1)"
+    New-Item -Path $HOME\profile.local.ps1 -ItemType File
+}
